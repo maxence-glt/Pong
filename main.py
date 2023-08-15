44986c39e901116e5849cf0f33431ab3e25d545d
maxence-glt <maxencegilloteaux2023@gmail.com>
2023-08-15 19:46:44 -0400

    score and collisions work!

--- main.py
+++ main.py
@@ -23,9 +23,10 @@ opponent_speed = 0
 
 
 # Player setup
+racket_height = 140
 x, y = 70, 330
 player_speed = 0
-player_rect = pygame.Rect(10, (height / 2) - 70, 10, 140)
+player_rect = pygame.Rect(30, (height / 2) - 70, 10, racket_height)
 
 def player_limits():
     if player_rect.top <= 0:
@@ -38,7 +39,7 @@ def player_limits():
 
 
 # Opponent setup
-opponent_rect = pygame.Rect(width - 20, (height / 2) - 70, 10, 140)
+opponent_rect = pygame.Rect(width - 30, (height / 2) - 70, 10, racket_height)
 
 def opponent_limits():
     if opponent_rect.top <= 0:
@@ -50,22 +51,39 @@ def opponent_limits():
 
 
 # Ball
-ball_rect = pygame.Rect(width/2 - 10, height /2 - 10, 20, 20)
+ball_rect = pygame.Rect(width/2, height /2, 20, 20)
 ball_speed_x = 0
 ball_speed_y = 0
 
 
+def ball_movement():
+    global ball_speed_x, ball_speed_y
+    print(ball_speed_x)
+    if ball_rect.colliderect(player_rect): ball_speed_x = ball_speed_x * -1
+    if ball_rect.colliderect(opponent_rect): ball_speed_x = ball_speed_x * -1
+
+    ball_rect.x += ball_speed_x
+    ball_rect.y += ball_speed_y
 
 def ball_limits():
-    if ball_rect.x <= 0 or ball_rect.x >= width: exit()
-    if ball_rect.y == 0: x
-    if ball_rect.y == height: x
+    global opponent_score, player_score
+    if ball_rect.x <= -10: 
+        opponent_score += 1
+        ball_init()
+    if ball_rect.x >= width + 10: 
+        player_score += 1
+        ball_init()
+    if ball_rect.y == 0: ball_speed_y = ball_speed_y * -1
+    if ball_rect.y == height: ball_speed_y = ball_speed_y * -1
 
 def ball_init():
     global ball_speed_x
+    ball_speed_x = 0
+    ball_rect.x = width / 2 - 10
+    ball_rect.y = height / 2 - 10
     start = random.randint(0, 1)
-    if start == 0: ball_speed_x += 5
-    if start == 1: ball_speed_x -= 5
+    if start == 0: ball_speed_x -= 15
+    if start == 1: ball_speed_x += 15
     
 
 
@@ -74,21 +92,20 @@ def ball_init():
 player_score = 0
 opponent_score = 0
 score_font = pygame.font.Font("bit5x3.ttf", 80)
-pscore = score_font.render(f"{player_score}", False, "White")
-oscore = score_font.render(f"{opponent_score}", False, "White")
 
 
 
-
-ball_init()
 
 # Game loop
+ball_init()
+
 while True:
     
 
 
     # Event loop
     for event in pygame.event.get():
+        # X out of the game
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
@@ -124,16 +141,18 @@ while True:
     # Update
     screen.fill((0, 0, 0))
 
+    ball_movement()
+
     player_rect.y += player_speed
     opponent_rect.y += opponent_speed
 
-    ball_rect.x += ball_speed_x
-    ball_rect.y += ball_speed_y
+
 
     # Control
     player_limits()
     opponent_limits()
     ball_limits()
+
 
     # Draw
     pygame.draw.rect(screen, "White", player_rect)
@@ -141,8 +160,8 @@ while True:
     pygame.draw.ellipse(screen, "White", ball_rect)
     pygame.draw.aaline(screen, "White", (width / 2, 0),(width / 2, height))
 
-    screen.blit(pscore, ((width / 2) - 120, 50))
-    screen.blit(oscore, ((width / 2) + 100, 50))
+    screen.blit(score_font.render(f"{player_score}", False, "White"), ((width / 2) - 120, 50))
+    screen.blit(score_font.render(f"{opponent_score}", False, "White"), ((width / 2) + 100, 50))
 
 
 
